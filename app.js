595bca8368d16024b48c632f5fed65d782f5682a
Geoffrey-Maillot <geoffrey.maillot@gmail.com>
2020-12-31 00:39:08 +0100

    Projet version 1 finis--voir readme pour la suite

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
 const colorPicker = {
-  gridSize: 8,
+  gridSize: 10,
   element: null,
+  //propriété qui sert à stocké la couleurs du picker
   color: null,
-
-
-  // 
+  
+  
+  // Récupère les div de la palette et pose un écouteur
   getColor: function() {
     const blue = document.getElementById(`blue`)
     blue.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const red = document.getElementById(`red`)
     red.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const green = document.getElementById(`green`)
     green.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const purple = document.getElementById(`purple`)
     purple.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const orange = document.getElementById(`orange`)
     orange.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const grey = document.getElementById(`grey`)
     grey.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const yellow = document.getElementById(`yellow`)
     yellow.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const pink = document.getElementById(`pink`)
     pink.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const black = document.getElementById(`black`)
     black.addEventListener(`click`, colorPicker.saveColor)
-
+    
     const white = document.getElementById(`white`)
     white.addEventListener(`click`, colorPicker.saveColor)
- 
-     
+    
+    
   },
-
+  
+  //Sauvegarde la couleurs de la palette qui a été cliquer
   saveColor: function (event){
     colorPicker.color = event.target.id
     console.log(colorPicker.color)
   }, 
   
+  //Génère la grille
   generateGrill: function(gridSize) {
-    
-    const containerElement = document.getElementById(`containerGrill`);
-    
-    for (row = 1; row < gridSize; row++) {
+
+   
+    for (row = 0; row < gridSize; row++) {
       const createRow = document.createElement(`div`);
       createRow.className  = `row`;
-      containerElement.appendChild(createRow);
-      for(cell = 1; cell < gridSize; cell++) {
+      colorPicker.element.appendChild(createRow);
+      for(cell = 0; cell < gridSize; cell++) {
         const createCell = document.createElement(`div`);
         createCell.className = `cell`;
         createRow.appendChild(createCell);
@@ -60,17 +62,36 @@ const colorPicker = {
       }
     }
   },
-
+  //Applique la couleur à la cellule
   handlerChangeColor: function (event) {
-    console.log(event)
-    console.log(colorPicker.color)
-    event.target.id = colorPicker.color
+
+    if (event.target.id === colorPicker.color) {
+       event.target.id = ``
+    }
+    else(event.target.id = colorPicker.color)
+   
+  },
+
+  sizeGrill: function () {
+    formElem = document.getElementById(`configuration`);
+    formElem.addEventListener(`submit`, (event) => {
+
+      event.preventDefault()
+      const inputElem = document.getElementById(`size`)
+      colorPicker.gridSize = inputElem.valueAsNumber
+      inputElem.innerHTML= ``;
+      colorPicker.element.innerHTML = ``
+
+     colorPicker.generateGrill(colorPicker.gridSize)
+
+    } )
   },
   
   init: function() {
+    colorPicker.element = document.getElementById(`containerGrill`);
     colorPicker.generateGrill(colorPicker.gridSize)
     colorPicker.getColor()
-   
+    colorPicker.sizeGrill()
   },
 }
 
